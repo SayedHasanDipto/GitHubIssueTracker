04166976d8d135ae4df43e16f4334c53a6ec4990
SayedHasanDipto <sayedhasandipto@GMAIL.COM>
2026-03-08 21:05:04 +0600

    Adding issue card

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,43 +1,51 @@
-console.log('yep')
-
-const getApi = () => {
-    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data.data);
-        })
-        .catch((e) => console.log(e));
+const issueCont = document.getElementById('issueCont');
+// For load issue
+async function loadIssue() {
+    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const issues = await res.json();
+    // console.log(data); 
+    displayIssue(issues.data);
 }
-getApi();
-// Adding api data in container
-const issueCard = document.getElementById('issue_card');
-issueCard.innerHTML = `
-    <div class="p-4 border rounded-xl border-b-gray-200 border-l-gray-200 border-r-gray-200 border-t-green-600 border-t-4">
-                    <div class="border mb-1 rounded-lg border-gray-200 p-4">
-                        <div class="flex justify-between items-center">
+
+// For display issue by append in DOM
+const displayIssue = (trees) => {
+    // console.log(trees);
+    trees.forEach(tree => {
+        console.log(tree)
+        const issueContainer = document.createElement('div');
+        issueContainer.className = '';
+        issueContainer.innerHTML = `
+        <div class="p-4 border rounded-xl border-b-gray-200 border-l-gray-200 border-r-gray-200 border-t-4 shadow-sm">
+                    <div class="mb-1 rounded-lg p-4">
+                        <div class="flex justify-between items-center mb-4">
                             <img src="./assets/Open-Status.png" alt="openStatus">
-                            <button class="btn btn-soft btn-error rounded-full">HELP</button>
+                            <button id="btn-priority" class="btn btn-soft btn-error rounded-full">${tree.priority.toUpperCase()}</button>
                         </div>
-                        <div class="mb-6">
-                            <h1 class="font-semibold text-xl text-[#1F2937] mb-2">Fix navigation menu on mobile devices</h1>
-                            <p class="text-[#64748B]">The navigation menu doesn't collapse properly on mobile
-                                devices...</p>
+                        <div class="mb-3">
+                            <h1 class="font-semibold text-xl text-[#1F2937] mb-2">${tree.title}</h1>
+                            <p class="text-[#64748B]">${tree.description}</p>
                         </div>
                         <div class="flex gap-2 items-center mb-4">
-                            <button class="btn btn-error btn-soft rounded-full">
+                            <button class="btn btn-error text-lg btn-soft rounded-full">
                                 <i class="fa-solid fa-bug"></i>
                                 BUG
                             </button>
-                            <button class="btn btn-warning btn-soft rounded-full">
+                            <button class="btn btn-warning text-lg btn-soft rounded-full">
                                 <i class="fa-solid fa-life-ring"></i>
                                 HELP WANTED
                             </button>
                         </div>
                     </div>
-                    <div class="border rounded-lg border-gray-200 space-y-4 p-4">
+                    <hr/ class=" bg-gray-200 border-gray-200">
+                    <div class="rounded-lg space-y-4 p-4">
                         <p class="text-[#64748B] text-sm">#1 by john_doe</p>
                         <p class="text-[#64748B] text-sm">1/15/2024</p>
                     </div>
                 </div>
-`;
+                `;
+        issueCont.appendChild(issueContainer);
+
+    });
+}
+
+loadIssue();
